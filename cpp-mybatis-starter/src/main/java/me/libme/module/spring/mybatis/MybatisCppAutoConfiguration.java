a589292a48f66ca6753a38e6db1bee25e9bc91e9
3cgg@163.com <3cgg@163.com>
2018-10-09 09:24:41 +0800

    -

--- cpp-mybatis-starter/src/main/java/me/libme/module/spring/mybatis/MybatisCppAutoConfiguration.java
+++ cpp-mybatis-starter/src/main/java/me/libme/module/spring/mybatis/MybatisCppAutoConfiguration.java
@@ -28,17 +28,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 import java.util.List;
 
 @ConditionalOnClass({ SqlSessionFactory.class, SqlSessionFactoryBean.class })
-@ConditionalOnBean(DataSource.class)
 @EnableConfigurationProperties(MybatisCppProperties.class)
 @AutoConfigureAfter(org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration.class)
 public class MybatisCppAutoConfiguration {
